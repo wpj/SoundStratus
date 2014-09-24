051ab0989033ab34dd0a8394416f3ae9e441e839
Wyatt Johnston <wyjohnston@gmail.com>
2014-09-24 00:15:05 -0400

    Add public folder

---
+++ public/js/app.js
@@ -0,0 +1,93 @@
+angular.module('cirrusSounds', ['app.controllers', 'app.directives', 'app.filters', 'app.services', 'ui.router', 'satellizer'])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      template: '<h1>Welcome to Cirrus Sounds</h1>'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', '$auth', function($state, $auth) {
+        if ($auth.isAuthenticated()) { $state.go('trending'); }
+      }]
+    })
+    .state('trending', {
+      url: '/popular',
+      templateUrl: 'trending.html',
+      controller: 'NavCtrl',
+      onEnter: ['$state', '$auth', function($state, $auth) {
+        if (!$auth.isAuthenticated()) { $state.go('login'); }
+      }]
+    })
+    .state('trending.today', {
+      url: '/today',
+      templateUrl: 'trending-list.html',
+      controller: 'TrendingCtrl',
+      resolve: {
+        'timeframe': function() {
+          return {time: 'day'};
+        }
+      }
+    })
+    .state('trending.week', {
+      url: '/this-week',
+      templateUrl: 'trending-list.html',
+      controller: 'TrendingCtrl',
+      resolve: {
+        'timeframe': function() {
+          return {time: 'week'};
+        }
+      }
+    })
+    .state('trending.month', {
+      url: '/this-month',
+      templateUrl: 'trending-list.html',
+      controller: 'TrendingCtrl',
+      resolve: {
+        'timeframe': function() {
+          return {time: 'month'};
+        }
+      }
+    })
+    .state('trending.allTime', {
+      url: '/all-time',
+      templateUrl: 'trending-list.html',
+      controller: 'TrendingCtrl',
+      resolve: {
+        'timeframe': function() {
+          return {time: 'allTime'};
+        }
+      }
+    });
+
+  // $urlRouterProvider.otherwise('/');
+}])
+
+.config(['$authProvider', 'clientId', function($authProvider, clientId) {
+
+  $authProvider.oauth2({
+    name: 'soundcloud',
+    url: '/auth/soundcloud',
+    clientId: clientId,
+    redirectUri: window.location.origin,
+    authorizationEndpoint: 'https://soundcloud.com/connect'
+  });
+
+}])
+
+.run(['$rootScope', function($rootScope) {
+  $rootScope.messages = {};
+
+  $rootScope.$on('$stateChangeSuccess', function(evt) {
+    $rootScope.messages = {};
+  });
+}])
+
+.run(['clientId', function(clientId) {
+  SC.initialize({
+    client_id: clientId
+  });
+}]);
---
+++ public/js/controllers.js
@@ -0,0 +1,83 @@
+angular.module('app.controllers', [])
+.controller('MainCtrl', ['$scope', '$http', '$auth', 'Account', '$state', function($scope, $http, $auth, Account, $state) {
+
+  $scope.login = function() {
+    return $auth.authenticate('soundcloud')
+      .then(function(response) {
+        $scope.getProfile()
+        .then(function() { $state.go('trending'); });
+      })
+      .catch(function(response) {
+        console.log("Error:", response);
+      });
+  };
+
+  $scope.logout = function() {
+    return $auth.logout()
+      .then(function() {
+        $scope.user = null;
+        $state.go('login');
+      });
+  };
+
+  $scope.isAuthenticated = function() {
+    return $auth.isAuthenticated();
+  };
+
+  $scope.getProfile = function() {
+    return Account.get()
+      .then(function(response) {
+        $scope.user = response.data;
+        return $scope.user;
+      })
+      .catch(function(error) {
+        return console.log(error);
+      });
+  };
+
+}])
+
+.controller('AuthCtrl', ['$scope', '$http', function($scope, $http) {
+
+}])
+
+.controller('NavCtrl', ['$rootScope', '$scope', '$state', 'Soundcloud', function($rootScope, $scope, $state, Soundcloud) {
+
+  $state.go('trending.week');
+
+  $scope.activeTab = function(tab) {
+    return tab === $state.current.name;
+  };
+
+  $scope.$on('data:notFound', function() {
+    $rootScope.messages.noDataFound = true;
+  });
+
+  $scope.$on('data:follow', function() {
+    $rootScope.messages.follow = true;
+  });
+
+  $scope.$on('data:error', function(evt, error) {
+    console.log(error);
+    $rootScope.messages.error = error.data.errors;
+  });
+}])
+
+.controller('TrendingCtrl', ['$scope', 'Soundcloud', 'timeframe', '$state', function($scope, Soundcloud, timeframe, $state) {
+
+  $scope.getProfile().then(function(user) {
+    Soundcloud.parseUser(user.uid, timeframe.time)
+    .then(function(songs) {
+      $scope.songs = songs;
+      if (!songs.length) {
+        $scope.$emit('data:notFound');
+      } else if (songs.length < 10) {
+        $scope.$emit('data:follow');
+      }
+    })
+    .catch(function(err) {
+      if (err) $scope.$emit('data:error', err);
+    });
+  });
+
+}]);
