2cf9d61a1d1f7644802b39913a41a154dd4d847c
Wyatt Johnston <wyjohnston@gmail.com>
2014-09-30 21:19:54 -0400

    Remove dev adjustments

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -75,82 +75,82 @@ angular.module('app.controllers', [])
 
   $scope.showLoading = false;
 
-  // if (musicCache.cached) {
-  //   $q.when(Soundcloud.filterByTime(musicCache.get(), timeframe.time))
-  //     .then(function(songs) {
-  //       $scope.showLoading = false;
-  //       $scope.songs = songs;
-  //       if (!songs.length) {
-  //         $scope.$emit('data:notFound');
-  //       } else if (songs.length < 10) {
-  //         $scope.$emit('data:follow');
-  //       }
-  //     })
-  //     .catch(function(error) {
-  //       $scope.showLoading = false;
-  //       if (error) $scope.$emit('data:error', error);
-  //     });
-  // } else {
-  //   $scope.showLoading = true;
-  //   $scope.getProfile().then(function(user) {
+  if (musicCache.cached) {
+    $q.when(Soundcloud.filterByTime(musicCache.get(), timeframe.time))
+      .then(function(songs) {
+        $scope.showLoading = false;
+        $scope.songs = songs;
+        if (!songs.length) {
+          $scope.$emit('data:notFound');
+        } else if (songs.length < 10) {
+          $scope.$emit('data:follow');
+        }
+      })
+      .catch(function(error) {
+        $scope.showLoading = false;
+        if (error) $scope.$emit('data:error', error);
+      });
+  } else {
+    $scope.showLoading = true;
+    $scope.getProfile().then(function(user) {
       
-  //     Soundcloud.parseUser(user.uid)
-  //       .then(function(songs) {
-  //         $scope.showLoading = false;
-  //         musicCache.set(songs);
+      Soundcloud.parseUser(user.uid)
+        .then(function(songs) {
+          $scope.showLoading = false;
+          musicCache.set(songs);
 
-  //         $q.when(Soundcloud.filterByTime(songs, timeframe.time))
-  //           .then(function(filteredSongs) {
-  //             $scope.songs = filteredSongs;
-  //             if (!filteredSongs.length) {
-  //               $scope.$emit('data:notFound');
-  //             } else if (filteredSongs.length < 10) {
-  //               $scope.$emit('data:follow');
-  //             }
-  //           })
-  //           .catch(function(error) {
-  //             if (error) $scope.$emit('data:error', error);
-  //           });
-  //       })
-  //       .catch(function(err) {
-  //         if (err) $scope.$emit('data:error', err);
-  //       });
-  //   });
-  // }
+          $q.when(Soundcloud.filterByTime(songs, timeframe.time))
+            .then(function(filteredSongs) {
+              $scope.songs = filteredSongs;
+              if (!filteredSongs.length) {
+                $scope.$emit('data:notFound');
+              } else if (filteredSongs.length < 10) {
+                $scope.$emit('data:follow');
+              }
+            })
+            .catch(function(error) {
+              if (error) $scope.$emit('data:error', error);
+            });
+        })
+        .catch(function(err) {
+          if (err) $scope.$emit('data:error', err);
+        });
+    });
+  }
 
   // $scope.$on('$destroy', function() {
   //   $scope.songs = null;
   // });
 
   
-  $scope.songs = [
-    {
-      id: 168479631,
-      permalink_url: "http://soundcloud.com/sbtrkt/sbtrkt-the-light-feat-denai-moore",
-      title: "SBTRKT - THE LIGHT ft Denai Moore",
-      user: {
-        permalink_url: "http://soundcloud.com/sbtrkt",
-        username: "SBTRKT"
-      }
-    },
-    {
-      id: 168992385,
-      permalink_url: "http://soundcloud.com/four-tet/john-beltran-faux-four-tet-remixtext033",
-      title: "John Beltran - Faux (Four Tet Remix)TEXT033",
-      user: {
-        permalink_url: "http://soundcloud.com/four-tet",
-        username: "Four Tet"
-      }
-    },
-    {
-      id: 168460044,
-      permalink_url: "http://soundcloud.com/xlr8r/kidkanevil-thousand-year-forest-one-for-yosi",
-      title: "kidkanevil - Thousand Year Forest (One for Yosi)",
-      user: {
-        permalink_url: "http://soundcloud.com/xlr8r",
-        username: "XLR8R"
-      }
-    }
-  ];
+  // $scope.songs = [
+  //   {
+  //     id: 168479631,
+  //     permalink_url: "http://soundcloud.com/sbtrkt/sbtrkt-the-light-feat-denai-moore",
+  //     title: "SBTRKT - THE LIGHT ft Denai Moore",
+  //     user: {
+  //       permalink_url: "http://soundcloud.com/sbtrkt",
+  //       username: "SBTRKT"
+  //     }
+  //   },
+  //   {
+  //     id: 168992385,
+  //     permalink_url: "http://soundcloud.com/four-tet/john-beltran-faux-four-tet-remixtext033",
+  //     title: "John Beltran - Faux (Four Tet Remix)TEXT033",
+  //     user: {
+  //       permalink_url: "http://soundcloud.com/four-tet",
+  //       username: "Four Tet"
+  //     }
+  //   },
+  //   {
+  //     id: 168460044,
+  //     permalink_url: "http://soundcloud.com/xlr8r/kidkanevil-thousand-year-forest-one-for-yosi",
+  //     title: "kidkanevil - Thousand Year Forest (One for Yosi)",
+  //     user: {
+  //       permalink_url: "http://soundcloud.com/xlr8r",
+  //       username: "XLR8R"
+  //     }
+  //   }
+  // ];
 
 }]);
