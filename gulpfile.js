1335c5a059663857fe194b26110809d49a5a6214
Wyatt Johnston <wyjohnston@gmail.com>
2014-09-30 21:25:41 -0400

    Concat JS for deploy

--- gulpfile.js
+++ gulpfile.js
@@ -39,9 +39,9 @@ gulp.task('browser-sync', function() {
 gulp.task('js', function() {
   gulp.src(['src/js/**/*'])
   .pipe(sourcemaps.init())
-    // .pipe(concat('app.js'))
-    // .pipe(uglify())
   .pipe(gulpIf(/[.]coffee$/, coffee().on('error', gutil.log)))
+  .pipe(concat('app.js'))
+  .pipe(uglify())
   .pipe(sourcemaps.write())
   .pipe(gulp.dest(dist.directory + 'js/'))
   .pipe(reload({stream: true}));
