26f35a839b551fa0a0929ec4e456bca2763c4f16
Wyatt Johnston <wyjohnston@gmail.com>
2014-10-01 21:21:16 -0400

    Handle all errors during user parsing

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -76,103 +76,100 @@ angular.module('app.controllers', [])
   $scope.activeTab = function(tab) {
     return tab === $state.current.name;
   };
-
-  $scope.$on('data:notFound', function() {
-    $rootScope.messages.noDataFound = true;
-  });
-
-  $scope.$on('data:follow', function() {
-    $rootScope.messages.follow = true;
-  });
-
-  $scope.$on('data:error', function(evt, error) {
-    console.log(error);
-    $rootScope.messages.error = error.data.errors;
-  });
 }])
 
 .controller('TrendingCtrl',
-  ['$scope', '$q', 'Soundcloud', 'musicCache', 'timeframe', '$state',
-  function($scope, $q, Soundcloud, musicCache, timeframe, $state) {
+  ['$rootScope', '$scope', '$q', 'Soundcloud', 'musicCache', 'timeframe', '$state',
+  function($rootScope, $scope, $q, Soundcloud, musicCache, timeframe, $state) {
 
-  // $scope.showLoading = false;
+  $scope.showLoading = false;
 
-  // if (musicCache.cached) {
-  //   $q.when(Soundcloud.filterByTime(musicCache.get(), timeframe.time))
-  //     .then(function(songs) {
-  //       $scope.showLoading = false;
-  //       $scope.songs = songs;
-  //       if (!songs.length) {
-  //         $scope.$emit('data:notFound');
-  //       } else if (songs.length < 10) {
-  //         $scope.$emit('data:follow');
-  //       }
-  //     })
-  //     .catch(function(error) {
-  //       $scope.showLoading = false;
-  //       if (error) $scope.$emit('data:error', error);
-  //     });
-  // } else {
-  //   $scope.showLoading = true;
-  //   $scope.getProfile().then(function(user) {
-      
-  //     Soundcloud.parseUser(user.uid)
-  //       .then(function(songs) {
-  //         $scope.showLoading = false;
-  //         musicCache.set(songs);
+  if (musicCache.cached) {
+    $q.when(Soundcloud.filterByTime(musicCache.get(), timeframe.time))
 
-  //         $q.when(Soundcloud.filterByTime(songs, timeframe.time))
-  //           .then(function(filteredSongs) {
-  //             $scope.songs = filteredSongs;
-  //             if (!filteredSongs.length) {
-  //               $scope.$emit('data:notFound');
-  //             } else if (filteredSongs.length < 10) {
-  //               $scope.$emit('data:follow');
-  //             }
-  //           })
-  //           .catch(function(error) {
-  //             if (error) $scope.$emit('data:error', error);
-  //           });
-  //       })
-  //       .catch(function(err) {
-  //         if (err) $scope.$emit('data:error', err);
-  //       });
-  //   });
-  // }
+      .then(function(songs) {
+        $scope.showLoading = false;
+        $scope.songs = songs;
+        if (!songs.length) {
+          $rootScope.messages.noDataFound = true;
+        } else if (songs.length < 10) {
+          $rootScope.messages.follow = true;
+        }
+      })
+
+      .catch(function(error) {
+        $scope.showLoading = false;
+        $rootScope.messages.error = error.data.errors;
+      });
+
+  } else {
+    $scope.showLoading = true;
+    $scope.getProfile()
+      .then(function(user) {
+        Soundcloud.parseUser(user.uid)
+
+          .then(function(songs) {
+            $scope.showLoading = false;
+            musicCache.set(songs);
+            $q.when(Soundcloud.filterByTime(songs, timeframe.time))
+
+              .then(function(filteredSongs) {
+                $scope.songs = filteredSongs;
+                if (!filteredSongs.length) {
+                  $rootScope.messages.noDataFound = true;
+                } else if (filteredSongs.length < 10) {
+                  $rootScope.messages.follow = true;
+                }
+              })
+
+              .catch(function(error) {
+                $rootScope.messages.error = error.data.errors;
+              });
+          })
+
+          .catch(function(error) {
+            $scope.showLoading = false;
+            $rootScope.messages.error = error.data.errors;
+          });
+      })
+      .catch(function(error) {
+        $rootScope.messages.error = error.data.errors;
+      });
+  }
 
   // $scope.$on('$destroy', function() {
   //   $scope.songs = null;
   // });
 
   
-  $scope.songs = [
-    {
-      id: 168479631,
-      permalink_url: "http://soundcloud.com/sbtrkt/sbtrkt-the-light-feat-denai-moore",
-      title: "SBTRKT - THE LIGHT ft Denai Moore",
-      user: {
-        permalink_url: "http://soundcloud.com/sbtrkt",
-        username: "SBTRKT"
-      }
-    },
-    {
-      id: 168992385,
-      permalink_url: "http://soundcloud.com/four-tet/john-beltran-faux-four-tet-remixtext033",
-      title: "John Beltran - Faux (Four Tet Remix)TEXT033",
-      user: {
-        permalink_url: "http://soundcloud.com/four-tet",
-        username: "Four Tet"
-      }
-    },
-    {
-      id: 168460044,
-      permalink_url: "http://soundcloud.com/xlr8r/kidkanevil-thousand-year-forest-one-for-yosi",
-      title: "kidkanevil - Thousand Year Forest (One for Yosi)",
-      user: {
-        permalink_url: "http://soundcloud.com/xlr8r",
-        username: "XLR8R"
-      }
-    }
-  ];
+  // $scope.songs = [
+  //   {
+  //     id: 168479631,
+  //     permalink_url: "http://soundcloud.com/sbtrkt/sbtrkt-the-light-feat-denai-moore",
+  //     title: "SBTRKT - THE LIGHT ft Denai Moore",
+  //     user: {
+  //       permalink_url: "http://soundcloud.com/sbtrkt",
+  //       username: "SBTRKT"
+  //     }
+  //   },
+  //   {
+  //     id: 168992385,
+  //     permalink_url: "http://soundcloud.com/four-tet/john-beltran-faux-four-tet-remixtext033",
+  //     title: "John Beltran - Faux (Four Tet Remix)TEXT033",
+  //     user: {
+  //       permalink_url: "http://soundcloud.com/four-tet",
+  //       username: "Four Tet"
+  //     }
+  //   },
+  //   {
+  //     id: 168460044,
+  //     permalink_url: "http://soundcloud.com/xlr8r/kidkanevil-thousand-year-forest-one-for-yosi",
+  //     title: "kidkanevil - Thousand Year Forest (One for Yosi)",
+  //     user: {
+  //       permalink_url: "http://soundcloud.com/xlr8r",
+  //       username: "XLR8R"
+  //     }
+  //   }
+  // ];
 
 }]);
